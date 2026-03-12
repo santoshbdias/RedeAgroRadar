ac9ee2be3e20aa11d79c59ca2f5dcef574f93bbb
SantosHBDias <santoshbdias@gmail.com>
2026-03-12 13:11:22 -0300

    Commit inicial SHBD

--- R/hello.R
+++ R/hello.R
@@ -15,47 +15,4 @@
 
 
 
-#############################################################################################
-#########################CRIANDO MEU PACOTE#################################################
-#
 
-
-## Adiciona o pacote httr como dependencia oficial
-#usethis::use_package("httr")
-#usethis::use_package("magick")
-#usethis::use_package("shiny")
-#usethis::use_package("bslib")
-
-#rm(list = ls()); gc(); graphics.off(); cat("\014")#Limpar todos os dados e abas
-
-
-# Atualiza os arquivos de documentacao com a nova funcao exportada
-#devtools::document()
-
-# Faz o pacote ficar disponivel para voce testar agora mesmo
-#devtools::load_all()
-
-# Testa o aplicativo!
-#Run_Monitor_RedeAgro(Token='7935384745:AAET5JvZdH6qCnpfPrMEi-plgrVMHEx_Eo8',chatID='-4952627577', limites_personalizados = list(vr = 70, vb = 25, ar = 65, ab = 33)) # Você pode omitir o amarelo (ar, ab), ele puxa o padrão!
-
-Run_Monitor_RedeAgro(Token='7935384745:AAET5JvZdH6qCnpfPrMEi-plgrVMHEx_Eo8',
-                     chatID='-4952627577',
-                     limites_personalizados = list(vr = 70, vb = 25, ar = 65, ab = 33)) # Você pode omitir o amarelo (ar, ab), ele puxa o padrão!
-
-Run_Monitor_RedeAgro(Token='7935384745:AAET5JvZdH6qCnpfPrMEi-plgrVMHEx_Eo8',chatID='-4952627577')
-
-
-#PUBLICAÇÃO CRAN
-#devtools::check_win_release()
-#devtools::check_win_devel()
-
-#usethis::use_cran_comments()
-
-#devtools::spell_check()
-
-#devtools::release()
-
-#
-# hello <- function() {
-#   print("Hello, world!")
-# }
